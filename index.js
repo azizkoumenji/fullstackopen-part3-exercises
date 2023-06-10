54acebfcdb2150f8340fcb98926360859e0c1af4
Mohamed Aziz Koumenji <113291493+mohamedazizkoumenji@users.noreply.github.com>
2023-06-10 22:09:13 +0100

    Added build directory and linked the backend

---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require("express");
+let morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.static("build"));
+
+morgan.token("data", (req, res) => {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
+
+let phonebook = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons", (request, response) => {
+  response.json(phonebook);
+});
+
+app.get("/info", (request, response) => {
+  response.send(
+    `<p>The phonebook has info for ${
+      phonebook.length
+    } people.<p>${new Date()}<p>`
+  );
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const data = phonebook.find((person) => person.id === id);
+  if (data) {
+    response.json(data);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  phonebook = phonebook.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number is missing",
+    });
+  } else if (phonebook.find((person) => person.name === body.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
+    id: Math.random() * 1000,
+    name: body.name,
+    number: body.number,
+  };
+
+  phonebook = phonebook.concat(person);
+
+  response.json(person);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
